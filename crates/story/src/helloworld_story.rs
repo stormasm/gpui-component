5855662f21c71b8d128ba922155ead8e273fa746
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2024-10-15 20:14:22 -0700

    reduce the number of things going on the screen

--- crates/story/src/helloworld_story.rs
+++ crates/story/src/helloworld_story.rs
@@ -1,16 +1,9 @@
 use gpui::{
-    div, CursorStyle, InteractiveElement, ParentElement, Render, StatefulInteractiveElement,
-    Styled, View, ViewContext, VisualContext as _, WindowContext,
+    CursorStyle, InteractiveElement, ParentElement, Render, StatefulInteractiveElement, Styled,
+    View, ViewContext, VisualContext as _, WindowContext,
 };
 
-use ui::{
-    button::{Button, ButtonStyle},
-    checkbox::Checkbox,
-    h_flex,
-    label::Label,
-    tooltip::Tooltip,
-    v_flex,
-};
+use ui::{h_flex, label::Label, tooltip::Tooltip, v_flex};
 
 pub struct HwStory {
     focus_handle: gpui::FocusHandle,
@@ -48,38 +41,13 @@ impl gpui::FocusableView for HwStory {
 }
 impl Render for HwStory {
     fn render(&mut self, _: &mut gpui::ViewContext<Self>) -> impl gpui::IntoElement {
-        v_flex()
-            .p_4()
-            .gap_5()
-            .child(
-                div()
-                    .cursor(CursorStyle::PointingHand)
-                    .child(
-                        Button::new("button")
-                            .label("Hover over me Hello World")
-                            .style(ButtonStyle::Primary),
-                    )
-                    .id("tooltip-1")
-                    .tooltip(|cx| Tooltip::new("This is a Button", cx)),
-            )
-            .child(
-                h_flex()
-                    .justify_center()
-                    .cursor(CursorStyle::PointingHand)
-                    .child(Label::new("Hover near me Hello World"))
-                    .id("tooltip-3")
-                    .tooltip(|cx| Tooltip::new("This is a Label", cx)),
-            )
-            .child(
-                div()
-                    .cursor(CursorStyle::PointingHand)
-                    .child(
-                        Checkbox::new("check")
-                            .label("Remember me or not")
-                            .checked(true),
-                    )
-                    .id("tooltip-4")
-                    .tooltip(|cx| Tooltip::new("Roger Houston Check Two!", cx)),
-            )
+        v_flex().p_4().gap_5().child(
+            h_flex()
+                .justify_center()
+                .cursor(CursorStyle::PointingHand)
+                .child(Label::new("Hello World"))
+                .id("tooltip-1")
+                .tooltip(|cx| Tooltip::new("This is a Beautiful place !", cx)),
+        )
     }
 }
